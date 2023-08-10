0c9214ffc403956ef83b92ba4957359ba9a950f4
luongdinhduc0000 <luongdinhduc0000@gmail.com>
2023-08-10 14:17:00 +0700

    update

--- src/main/java/com/example/english_mcqbank/service/ExamTopicService.java
+++ src/main/java/com/example/english_mcqbank/service/ExamTopicService.java
@@ -5,6 +5,7 @@ import com.example.english_mcqbank.model.ExamTopic;
 import com.example.english_mcqbank.repository.ExamTopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,7 +18,12 @@ public class ExamTopicService {
         return examTopicRepository.findAllByExam(exam);
     }
 
+    public void deleteAllByExamId(int id) {
+        examTopicRepository.deleteAllByExamNative(id);
+    }
+
+    @Transactional
     public void deleteAllByExam(Exam exam) {
-        examTopicRepository.deleteAllByExamNative(exam.getId());
+        examTopicRepository.deleteAllByExam(exam);
     }
 }
