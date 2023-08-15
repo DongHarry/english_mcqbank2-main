d355ac53bed387b3de9b7d2ffc0c44c7f1969e6c
duc <luongdinhduc0000@gmail.com>
2023-08-15 22:20:57 +0700

    update

--- src/main/java/com/example/english_mcqbank/controller/api/QuestionRestController.java
+++ src/main/java/com/example/english_mcqbank/controller/api/QuestionRestController.java
@@ -12,24 +12,24 @@ import java.util.List;
 @RequestMapping("/api/questions")
 public class QuestionRestController {
     @Autowired
-    private IQuestionService IQuestionService;
+    private IQuestionService questionService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity<List<Question>> list() {
-        List<Question> questions = IQuestionService.getAllQuestions();
+        List<Question> questions = questionService.getAllQuestions();
         return ResponseEntity.ok(questions);
     }
 
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Question> get(@PathVariable int id) {
-        Question question = IQuestionService.get(id);
+        Question question = questionService.get(id);
         return ResponseEntity.ok(question);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Question> delete(@PathVariable int id) {
-        Question question = IQuestionService.get(id);
+        Question question = questionService.get(id);
         return ResponseEntity.ok(question);
     }
 
@@ -37,13 +37,13 @@ public class QuestionRestController {
     public ResponseEntity<List<Question>> getRandom(@RequestParam(defaultValue = "1") int topicId,
                                                     @RequestParam(defaultValue = "1") int level,
                                                     @RequestParam(defaultValue = "1") int number) {
-        List<Question> questions = IQuestionService.getRandom(topicId, level, number);
+        List<Question> questions = questionService.getRandom(topicId, level, number);
         return ResponseEntity.ok(questions);
     }
 
     @RequestMapping("/save")
     public ResponseEntity<Question> save(@RequestBody Question question) {
-        Question question1 = IQuestionService.save(question);
+        Question question1 = questionService.save(question);
         return ResponseEntity.ok(question1);
     }
 
